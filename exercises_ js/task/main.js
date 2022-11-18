de674d167eb31fed7eca19b3af323c337aba486f
Gabriel Castro <gaboj0531@gmail.com>
2022-11-17 20:33:31 -0400

    Guardo Cambios del archivo Task

--- exercises_ js/task/main.js
+++ exercises_ js/task/main.js
@@ -1,39 +1,26 @@
 
 let write = document.querySelector('.toWrite');
 let addSend = document.querySelector('.sendTask');
+let boton = document.querySelectorAll('.showTask');
 const show = document.querySelector('.show');
+const eraseButtons = document.querySelectorAll('.btn_delete');
 
 addSend.addEventListener('click', addTask);
 function addTask(){
     let div = `<div class="showTask">${write.value}<input class="checkbox" type="checkbox"><button class="btn_delete">remove</button></div>`;
     div.innerHTML;
     console.log(`Hiciste click y este estu valor ${div}`);    
-    show.insertAdjacentHTML('afterbegin', div)
-
-    const btnRemove = document.querySelectorAll('.btn_delete');
-    let boton = document.querySelectorAll('.showTask');
-    btnRemove.forEach(btn=>{
-        btn.removeEventListener('click', addTask)
-        btn.addEventListener('click', ()=>{
-            boton.forEach(b=>{
-                b.remove();
-            })
-            console.log('Hiciste click sobre este boton');
-        })
-    })
+    show.insertAdjacentHTML('afterbegin', div);
+}
+ 
+function removeTask(event){
+    const target = event.currentTarget;
 }
 
-
-
-// btnRemove.addEventListener('click', removeTask); 
-//     function removeTask(){
-//         btnRemove.forEach(btn=>{
-//             console.log('Hiciste click');
-//         })
-//     } 
-
-   
-
+eraseButtons.forEach(btn=> {
+    btn.addEventListener('click', removeTask);
+ }
+    
 
 
 
